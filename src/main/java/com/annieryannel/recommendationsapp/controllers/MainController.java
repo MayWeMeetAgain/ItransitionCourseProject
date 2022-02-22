582c98d982c308eb66892cc8e8203c3fdd90ff2a
Annie Ryannel <ryannel321@gmail.com>
2022-02-23 01:48:56 +0300

    initial commit

---
+++ src/main/java/com/annieryannel/recommendationsapp/controllers/MainController.java
@@ -0,0 +1,17 @@
+package com.annieryannel.recommendationsapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String userAction(Model model) {
+        return "home";
+    }
+}
