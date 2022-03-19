3f2939b71dfd4b2e638c606130838bbfb3a561ff
Annie Ryannel <ryannel321@gmail.com>
2022-03-19 05:57:03 +0300

    properties improved

--- src/main/java/com/annieryannel/recommendationsapp/config/WebSecurityConfig.java
+++ src/main/java/com/annieryannel/recommendationsapp/config/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-//                .cors().and().csrf().disable()
+                .cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/registration").permitAll()
                 .antMatchers("/").permitAll()
